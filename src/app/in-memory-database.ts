0ed648ce8a6f6a6572654d9ef73541bba202125b
bbeatriz <brunabeatrizncosta@gmail.com>
2021-02-25 17:43:06 -0300

    Adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,6 +1,7 @@
 import { InMemoryDbService, RequestInfo } from "angular-in-memory-web-api";
 import { SSL_OP_NO_TLSv1_1 } from "constants";
 import { Observable } from "rxjs";
+import { Category } from "./pages/categories/shared/category.model";
 
 export class InMemoryDatabase implements InMemoryDbService {
   
